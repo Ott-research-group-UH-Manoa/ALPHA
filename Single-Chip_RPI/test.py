6f149b1dd39ef8914cdda30f9d067529605b1ae2
Lawrence Zheng <zheng3@hawaii.edu>
2025-09-04 09:32:48 -1000

    Added pygame test program to test displaying pygame GUI over ssh - Did not work

--- Single-Chip_RPI/test.py
+++ Single-Chip_RPI/test.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
+import os
+os.environ["SDL_VIDEODRIVER"] = "x11"   # set BEFORE importing pygame
+os.environ["SDL_AUDIODRIVER"] = "dummy"
+os.environ["LIBGL_ALWAYS_SOFTWARE"] = "1"
+os.environ["SDL_RENDER_DRIVER"] = "software"
+# optional: ensure we don't accidentally try Wayland
+os.environ.pop("WAYLAND_DISPLAY", None)
+os.environ["XDG_SESSION_TYPE"] = "x11"
 
-import pyvisa
-
-print("Creating ResourceManager...")
-rm = pyvisa.ResourceManager( '@py' )
-
-print("Listing resources...")
-print(rm.list_resources())  # Check if your instrument shows up here
-
-print("Opening connection...")
-inst = rm.open_resource('USB0::2391::1031::MY44013842::0::INSTR')
-
-print("Querying *IDN?...")
-print(inst.query("*IDN?"))  # Should return instrument ID string
-
-
-inst.write( "FREQ 1000000" )
-
-inst.write( "" )
+import pygame, sys
+print("DISPLAY =", os.environ.get("DISPLAY"))
+pygame.display.init()
+print("driver:", pygame.display.get_driver())
+screen = pygame.display.set_mode((300, 200))
+pygame.display.set_caption("Pygame over X11")
+running = True
+while running:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT: running = False
+    screen.fill((0, 100, 200))
+    pygame.display.flip()
+pygame.quit(); sys.exit()
